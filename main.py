e2d505066d99c191fc19797714e5ca0a9dee61b7
KhanhNguyen1308 <khanhnguyenduy1308@gmail.com>
2021-09-05 19:50:15 +0700

    update

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from ty_le_dau import ty_le_dau
 from ham import play_sound, draw_point, ty_le_mat
 from trang_thai_dau import gat_dau, trang_thai_dau, trang_thai_mat
 
-f = open("Text/avg.txt", "+w")
-m = open("Text/mode.txt", "+w")
+a = open("Text/n1.txt", "+w")
+b = open("Text/n2.txt", "+w")
+c = open("Text/c1.txt", "+w")
+d = open("Text/c2.txt", "+w")
+e = open("Text/avg.txt", "+w")
+f = open("Text/mode.txt", "+w")
 wav_path = '/home/pi/Documents/Drowsy_detect/alarm.wav'
 dem = 0
 gat_num = 0
@@ -23,7 +27,7 @@ mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh()
 drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
-cap = cv2.VideoCapture("Video/gg.mp4")
+cap = cv2.VideoCapture("Video/final_test.mp4")
 canh_bao = False
 while True:
     ret, img = cap.read()
@@ -47,12 +51,17 @@ while True:
             Right_eye.append([face[7], face[145], face[153], face[155], face[158], face[159]])
             img = draw_point(img, nose, Left_eye, Right_eye)
             ty_le_tb_mat = ty_le_mat(Left_eye, Right_eye)
-            f.write(str(round(ty_le_tb_mat,3))+"\n")
             nghieng_1, nghieng_2, cui_1, cui_2 = ty_le_dau(nose, Left_eye, Right_eye)
             tt_dau, mode = trang_thai_dau(nghieng_1, nghieng_2, cui_1, cui_2)
-            m.write(str(mode/10)+"\n")
             tt_mat, tt_mat_trc, dem, canh_bao= trang_thai_mat(ty_le_tb_mat, dem, mode, canh_bao, tt_mat_trc)
             gat_num, dem_gat, trang_thai_trc = gat_dau(trang_thai_trc, mode, dem_gat, gat_num, tt_mat)
+            a.write(str(round(nghieng_1,3))+"\n")
+            b.write(str(round(nghieng_2,3))+"\n")
+            c.write(str(round(cui_1,3))+"\n")
+            d.write(str(round(cui_2,3))+"\n")
+            e.write(str(round(ty_le_tb_mat,3))+"\n")
+            f.write(str(mode/10)+"\n")
+            z = "n1:"+str(round(nghieng_1,3))+"  n2:"+str(round(nghieng_2,3))+"  c1:"+str(round(cui_1,3))+"  c2:"+str(round(cui_2,3))
             if canh_bao == True:
                 color = (0, 0, 255)
                 cv2.putText(img, "CANH BAO!!!", (int(iw/2-250),int(ih/2)), cv2.FONT_HERSHEY_SIMPLEX, 3, color, 2)
@@ -73,7 +82,14 @@ while True:
     cv2.imshow('results', img)
     if key == ord('q'):
         break
+    if key == ord('a'):
+        print(z)
+
+a.close()
+b.close()
+c.close()
+d.close()
+e.close()
 f.close()
-m.close()
 cap.release()
 cv2.destroyAllWindows()
--- trang_thai_dau.py
+++ trang_thai_dau.py
@@ -1,53 +1,48 @@
-def so_sanh_va_canh_bao(ty_le, moc, dem):
-    if ty_le <= moc:
-        trang_thai = 'Nham'
-        dem += 1
-        if dem >= 20:
-            canh_bao = True
-    elif ty_le > moc:
-        trang_thai = 'Mo'
-        dem = 0
-        canh_bao = False
-    return trang_thai, canh_bao, dem
-
-
 def trang_thai_dau(nghieng_1, nghieng_2,cui_1, cui_2):
-    if 0.4 < nghieng_2 < 2:    
-        if 0.5 < nghieng_1 < 1.5:
-            if cui_1 <= 0.75: 
-                if cui_2 <= 0.75:
-                    trang_thai = 'Thang'
-                    mode = 0
-                elif cui_2 > 0.75:
-                    trang_thai = 'Cui'
-                    mode = 1
-            elif cui_1 > 0.75:
-                if cui_2 <= 0.75:
-                    trang_thai = 'Thang'
-                    mode = 0
-                elif cui_2 > 0.75:
-                    trang_thai = 'Cui'
-                    mode = 1
+    if 0.5 < nghieng_2 < 1.5:    
+        if 0.6 < nghieng_1 < 1.5:
+            if cui_1 >= 0.75 and cui_2 >= 0.75:
+                trang_thai = 'Cui'
+                mode = 1
+            else:
+                trang_thai = 'Thang'
+                mode = 0
 
-        elif nghieng_1 <= 0.5:
-            if cui_1 <= 0.3:
-                trang_thai = 'Nghieng phai'
-                mode = 2
-            elif cui_1 > 0.3:
-                trang_thai = 'Cui_Nghieng phai'
-                mode = 4
+        elif nghieng_1 <= 0.6:
+            if cui_1 < 1:
+                if nghieng_2 > 0.55:
+                    trang_thai = 'Nghieng phai'
+                    mode = 2
+                else:
+                    trang_thai = 'Cui nghieng phai'
+                    mode = 4
+            else:
+                if nghieng_2 < 0.55:
+                    trang_thai = 'Cui nghieng phai'
+                    mode = 4
+                else:
+                    trang_thai = 'Nghieng phai'
+                    mode = 2
 
         elif nghieng_1 >= 1.5:
-            if cui_2 <= 0.3:
-                trang_thai = 'Nghieng Trai'
-                mode = 3
-            elif cui_2 > 0.3:
-                trang_thai = 'Cui_Nghieng Trai'
-                mode = 5
-    elif nghieng_2 >= 2:
+            if cui_2 < 1:
+                if nghieng_2 < 1.25:
+                    trang_thai = 'Nghieng trai'
+                    mode = 3
+                else:
+                    trang_thai = 'Cui nghieng trai'
+                    mode = 5 
+            else:
+                if nghieng_2 > 0.75:
+                    trang_thai = 'Nghieng trai'
+                    mode = 3
+                else:
+                    trang_thai = 'Cui nghieng trai'
+                    mode = 5
+    elif nghieng_2 >= 1.5:
         trang_thai = 'Nhin Trai'
         mode = 7
-    elif nghieng_2 <= 0.4:
+    elif nghieng_2 <= 0.5:
         trang_thai = 'Nhin Phai'
         mode = 6
     return trang_thai, mode
@@ -60,21 +55,21 @@ def trang_thai_mat(ty_le_mat, dem, mode, canh_bao, trang_thai_trc):
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 1:
         if ty_le_mat <= 0.3:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 2:
         if ty_le_mat <= 0.28:
             trang_thai = 'Nham'
@@ -84,70 +79,70 @@ def trang_thai_mat(ty_le_mat, dem, mode, canh_bao, trang_thai_trc):
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 3:
         if ty_le_mat <= 0.28:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 4:
         if ty_le_mat <= 0.35:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Open'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 5:
         if ty_le_mat <= 0.3:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Open'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 6:
         if ty_le_mat <= 0.35:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                    Alarm = True
+                    canh_bao = True
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     elif mode == 7:
         if ty_le_mat <= 0.33:
             trang_thai = 'Nham'
             dem += 1
             if dem >= 20:
-                Alarm = True
+                canh_bao = True
         else:
             trang_thai = 'Mo'
             dem = 0
-            Alarm = False
+            canh_bao = False
     trang_thai_trc = trang_thai
     return trang_thai, trang_thai_trc, dem, canh_bao
 
 
-def gat_dau(prev_status, mode, dem, gat_num, trang_thai):
-    if prev_status == 0 and (mode == 0 or mode == 2 or mode == 3):
-        prev_status == mode
-    if mode == 1 or mode == 6 or mode == 7 and (prev_status == 0 or prev_status == 2 or prev_status == 3):
+def gat_dau(trang_thai_truoc, mode, dem, gat_num, trang_thai):
+    if trang_thai_truoc == 0 and (mode == 0 or mode == 2 or mode == 3):
+        trang_thai_truoc == mode
+    if mode == 1 or mode == 6 or mode == 7 and (trang_thai_truoc == 0 or trang_thai_truoc == 2 or trang_thai_truoc == 3):
         if trang_thai == "Nham":
             dem += 1
-            prev_status = mode
-    if (mode == 0 or mode == 2 or mode == 3) and (prev_status == 1 or prev_status == 6 or prev_status == 7):  
-        if dem <= 10 and dem != 0:
+            trang_thai_truoc = mode
+    if (mode == 0 or mode == 2 or mode == 3) and (trang_thai_truoc == 1 or trang_thai_truoc == 6 or trang_thai_truoc == 7):  
+        if dem <= 15 and dem != 0:
             gat_num += 1
             dem = 0
-    return gat_num, dem, prev_status
+    return gat_num, dem, trang_thai_truoc
